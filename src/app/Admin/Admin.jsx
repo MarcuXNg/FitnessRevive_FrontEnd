eced5fdbd4cffdf11d1b9b624b4555bd10373a83
quanhoang191003 <quanhoang191003@gmail.com>
2024-01-10 20:53:03 +0700

    update chart

--- src/app/Admin/Admin.jsx
+++ src/app/Admin/Admin.jsx
@@ -2,20 +2,144 @@ import React, {useEffect} from 'react';
 import Chart from 'react-apexcharts';
 
 const Admin = () => {
-  const MyChart = () => {
+  const Top5MealsChart = () => {
     const options = {
       // ApexCharts options go here
       chart: {
         type: 'bar',
       },
       xaxis: {
+        title: {
+          text: 'Month',
+        },
         categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+      },
+      yaxis: {
+        title: {
+          text: 'Products',
+        },
       },
     };
     const series = [
       {
         name: 'Sales',
         data: [30, 40, 45, 50, 49, 60, 70],
+      },
+      {
+        name: 'Revenue',
+        data: [76, 85, 101, 98, 87, 105, 91],
+      },
+    ];
+    return (
+      <Chart
+        options={options}
+        series={series}
+        type="bar"
+        height={350}
+      />
+    );
+  };
+  const CaloriesChart = () => {
+    const options = {
+      // ApexCharts options go here
+      chart: {
+        type: 'area',
+      },
+      xaxis: {
+        title: {
+          text: 'Month',
+        },
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      },
+      yaxis: {
+        title: {
+          text: 'Kcal',
+        },
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shadeIntensity: 1,
+          opacityFrom: 0.7,
+          opacityTo: 0.9,
+          stops: [0, 90, 100],
+        },
+      },
+    };
+    const series = [
+      {
+        name: 'Calories in',
+        data: [30, 140, 45, 450, 849, 160, 1070, 30, 140, 45, 450, 849],
+      },
+      {
+        name: 'Calories out',
+        data: [160, 640, 945, 350, 149, 460, 570, 160, 640, 945, 350, 149],
+      },
+    ];
+    return (
+      <Chart
+        options={options}
+        series={series}
+        type="area"
+        height={350}
+      />
+    );
+  };
+  const WeightChart = () => {
+    const options = {
+      // ApexCharts options go here
+      chart: {
+        type: 'bar',
+      },
+      xaxis: {
+        title: {
+          text: 'Month',
+        },
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      },
+      yaxis: {
+        title: {
+          text: 'Kg',
+        },
+      },
+    };
+    const series = [
+      {
+        name: 'Weight',
+        data: [100, 110, 95, 92, 89, 90, 88, 85, 82, 80, 79, 81],
+      },
+    ];
+    return (
+      <Chart
+        options={options}
+        series={series}
+        type="bar"
+        height={350}
+      />
+    );
+  };
+  const WaterDrinkChart = () => {
+    const options = {
+      // ApexCharts options go here
+      chart: {
+        type: 'bar',
+      },
+      xaxis: {
+        title: {
+          text: 'Month',
+        },
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+      },
+      yaxis: {
+        title: {
+          text: 'mL',
+        },
+      },
+    };
+    const series = [
+      {
+        name: 'Water drink',
+        data: [1300, 1440, 1645, 1550, 1149, 1560, 1470, 2000, 2500, 1600, 1400, 1500],
       },
     ];
     return (
@@ -70,12 +194,13 @@ const Admin = () => {
           {/* <!-- Top 5 meals and Calories --> */}
           <div className="charts-card">
             <h2 className="chart-title">Top 5 meals</h2>
-            <MyChart />
+            <Top5MealsChart />
             <div id="bar-chart"></div>
           </div>
 
           <div className="charts-card">
             <h2 className="chart-title">Calories</h2>
+            <CaloriesChart />
             <div id="area-chart"></div>
           </div>
         </div>
@@ -83,10 +208,12 @@ const Admin = () => {
         <div className="charts">
           <div className="charts-card">
             <h2 className="chart-title">Weight</h2>
+            <WeightChart />
             <div id="bar-chart1"></div>
           </div>
           <div className="charts-card">
             <h2 className="chart-title">Amount of drinking water</h2>
+            <WaterDrinkChart />
             <div id="bar-chart2"></div>
           </div>
         </div>
